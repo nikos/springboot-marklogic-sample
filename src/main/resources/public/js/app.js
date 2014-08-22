23cb31920d04b6b8c4186e676d81bac52d95ef4f
Niko Schmuck <niko@nava.de>
2014-08-22 17:13:13 +0200

    Basic CRU(without D) functionality using the JSON API.

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,3 +1,70 @@
+'use strict';
+
+var module = angular.module('MarkLogicRESTClientApp',
+    ['ngResource', 'ngRoute', 'toastr']
+);
+
+module.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '/views/list.html',
+            controller: 'ProductListController'
+        })
+        .when('/search', {
+            templateUrl: '/views/search.html',
+            controller: 'ApplicationController'
+        })
+        .when('/products/new', {
+            templateUrl: '/views/create.html',
+            controller: 'ProductCreateController'
+        })
+        .when('/products/:sku', {
+            templateUrl: '/views/detail.html',
+            controller: 'ProductDetailController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+module.factory('MarkLogicService', function ($resource) {
+    var service = $resource('/products/:sku.json', {id: '@sku'}, {
+        getProduct:    {method: 'GET'},
+        getProducts:   {method: 'GET',  url: '/products.json', isArray: true},
+        addProduct:    {method: 'POST', url: '/products' },
+        removeProduct: {method: 'DELETE'}
+    });
+    service.prototype.isNew = function() {
+        return (typeof(this.id) === 'undefined');
+    };
+    return service;
+});
+
+/* ======================================================================= */
+
+module.controller('ProductListController', function($scope, MarkLogicService) {
+    $scope.products = MarkLogicService.getProducts();
+});
+
+module.controller('ProductDetailController', function($scope, $routeParams, $location, MarkLogicService) {
+    var sku = $routeParams.sku;
+    $scope.product = MarkLogicService.getProduct({sku: sku});
+});
+
+module.controller('ProductCreateController', function($scope, $location, MarkLogicService, toastr) {
+
+    $scope.product = new MarkLogicService();
+
+    $scope.save = function () {
+        $scope.product.$addProduct(function (product, headers) {
+            toastr.success("Created new product");
+            $location.path('/');
+        });
+    };
+});
+
+/* ======================================================================= */
+
 function Hello($scope, $http) {
     $http.get('http://rest-service.guides.spring.io/greeting').
         success(function(data) {
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -1,27 +1,49 @@
-<!doctype html>
-<html ng-app>
+<!DOCTYPE html>
+<html ng-app="MarkLogicRESTClientApp">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sample web application: MarkLogic with SpringBoot</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.21/angular.min.js"></script>
-    <script src="/js/app.js"></script>
+    <link href="/bower/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/bower/angular-toastr/dist/angular-toastr.min.css" rel="stylesheet">
+    <style>
+        body {
+            padding-top: 60px;
+        }
+    </style>
 </head>
 
 <body>
-<p>
-    Huhu
-</p>
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="brand" href="#">MarkLogic Demo</a>
 
-<div ng-controller="Hello">
-    <p>The ID is {{greeting.id}}</p>
-    <p>The content is {{greeting.content}}</p>
+            <div class="nav-collapse collapse">
+                <ul class="nav" role="navigation">
+                    <li>
+                        <a href="#/products">Products</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
 
-Date: ${time?date}
-<br>
-Time: ${time?time}
-<br>
-Message: ${message}
+<div class="container">
+    <!-- this is where content will be injected by Angular -->
+    <div ng-view></div>
+</div>
 
+<script src="/bower/angular/angular.min.js"></script>
+<script src="/bower/angular-route/angular-route.min.js"></script>
+<script src="/bower/angular-resource/angular-resource.min.js"></script>
+<script src="/bower/angular-toastr/dist/angular-toastr.min.js"></script>
+<script src="/js/app.js"></script>
 </body>
 </html>
