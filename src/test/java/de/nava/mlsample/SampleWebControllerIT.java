746b6af03ae8b23c3eedfcdbfe36e80c0ebf3404
Niko Schmuck <niko@nava.de>
2014-08-30 13:25:37 +0200

    Added integration test for dynamic search query.

--- src/test/java/de/nava/mlsample/SampleWebControllerIT.java
+++ src/test/java/de/nava/mlsample/SampleWebControllerIT.java
@@ -1,5 +1,6 @@
 package de.nava.mlsample;
 
+import de.nava.mlsample.domain.Product;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,11 +11,13 @@ import org.springframework.http.*;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
 
 /**
  * Basic integration tests for FreeMarker application.
@@ -31,28 +34,38 @@ public class SampleWebControllerIT {
     @Value("${local.server.port}")
     private int port;
 
-    @Test
-    public void testFreeMarkerTemplate() throws Exception {
-        ResponseEntity<String> entity = new TestRestTemplate().getForEntity("http://localhost:" + port, String.class);
-        // TODO: add hamcrest contains string
-        assertEquals(HttpStatus.OK, entity.getStatusCode());
-        assertTrue("Wrong body:\n" + entity.getBody(), entity.getBody()
-                .contains("Sample web application: MarkLogic with SpringBoot"));
+    private RestTemplate template = new TestRestTemplate();
+
+    private String getBaseUrl() {
+        return "http://localhost:" + port;
     }
 
     @Test
-    public void testFreeMarkerErrorTemplate() throws Exception {
+    public void thatFreeMarkerIndexTemplateResponds() throws Exception {
+        ResponseEntity<String> entity = template.getForEntity(getBaseUrl(), String.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        assertThat(entity.getBody(), containsString("Sample web application: MarkLogic with SpringBoot"));
+    }
+
+    @Test
+    public void thatFreeMarkerErrorTemplateResponds() throws Exception {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
         HttpEntity<String> requestEntity = new HttpEntity<>(headers);
 
-        ResponseEntity<String> responseEntity = new TestRestTemplate().exchange(
-                "http://localhost:" + port + "/does-not-exist", HttpMethod.GET,
+        ResponseEntity<String> responseEntity = template.exchange(
+                getBaseUrl() + "/does-not-exist", HttpMethod.GET,
                 requestEntity, String.class);
 
         assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
-        assertTrue("Wrong body:\n" + responseEntity.getBody(), responseEntity.getBody()
-                .contains("Something went wrong: 404 Not Found"));
+        assertThat(responseEntity.getBody(), containsString("Something went wrong: 404 Not Found"));
+    }
+
+    @Test
+    public void thatProductsAsJSONResponds() {
+        ResponseEntity<Product[]> entity = template.getForEntity(getBaseUrl() + "/products.json", Product[].class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        assertEquals(10, entity.getBody().length);
     }
 
 }
---
+++ src/test/java/de/nava/mlsample/service/ProductRepositoryJSONIT.java
@@ -0,0 +1,46 @@
+package de.nava.mlsample.service;
+
+import com.marklogic.client.extra.jackson.JacksonHandle;
+import com.marklogic.client.io.Format;
+import com.marklogic.client.io.StringHandle;
+import com.marklogic.client.query.QueryManager;
+import com.marklogic.client.query.RawCombinedQueryDefinition;
+import de.nava.mlsample.MarkLogicSampleApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MarkLogicSampleApplication.class)
+public class ProductRepositoryJSONIT {
+
+    public static final String COLLECTION_REF = "/products.json";
+
+    @Autowired
+    protected QueryManager queryManager;
+
+    @Test
+    public void thatFindWithDynamicQueryOptionsReturnsValidResponse() throws IOException {
+        String optionsStr = FileCopyUtils.copyToString(
+                new FileReader("src/test/resources/queries/options-price-year.json"));
+        // construct query
+        StringHandle rawHandle = new StringHandle();
+        rawHandle.withFormat(Format.JSON).set(optionsStr);
+        RawCombinedQueryDefinition queryDef = queryManager.newRawCombinedQueryDefinition(rawHandle);
+        queryDef.setCollections(COLLECTION_REF);
+
+        JacksonHandle resultsHandle = new JacksonHandle();
+        queryManager.setPageLength(10);
+        queryManager.search(queryDef, resultsHandle);
+        int start = resultsHandle.get().get("start").intValue();
+        assertEquals("Start should be 1", 1, start);
+    }
+}
